7330be81cb00ec8bdf91a5d3d5b8fa461a28d50d
D33-5022-2023 <rehemamutua82@gmail.com>
2025-02-22 17:34:12 +0300

    Add files via upload

---
+++ 20250221-180947.cpp
@@ -0,0 +1,22 @@
+/*
+author:rehema mutua 
+reg no:d33-5022-2023
+program:to calculate simple interest
+date:20/2/2025
+*/
+#include<stdio.h>//printf()
+int main(){
+//variable declaration and initializition;
+int p;
+int r;
+int t;
+int i;
+printf("program to multiply three numbers;\n");
+      printf("enter the find number;\n");
+      scanf("%d",&p);
+      printf("enter the secon:\n");
+      scanf("%d",&r);
+      printf("enter the third number:\n");
+      scanf("%d",&t);
+      printf("I of%d",i);
+      return 0;}
