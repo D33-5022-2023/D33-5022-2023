da10fc853ed32916063b4f3fdf0ea3d0433dbdb1
D33-5022-2023 <rehemamutua82@gmail.com>
2025-02-22 17:42:34 +0300

    Add files via upload

---
+++ area .cpp
@@ -0,0 +1,20 @@
+/*
+author: rehema mutua 
+reg no:d33-5022-2023
+program:to find area of rectangle
+date:20/2/2025
+*/
+#include <stdio.h>//printf()
+int main(){
+//variable declaration and initializiation;
+int l;
+int w;
+int area;
+printf("program to multiply two numbers:\n");
+      printf("enter the first number:\n");
+      scanf("%d",&l);
+      printf("enter the second number:n");
+      scanf("%d",&w);
+      area=l*w;
+      printf("the area is %d",area);
+      return 0;}
